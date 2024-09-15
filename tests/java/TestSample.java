5969766750ec890792571a44318e7deb03c01c63
xuxoperezpallas <46867823+xuxoperezpallas@users.noreply.github.com>
2024-09-15 09:17:52 +0200

    Create TestSample.java

---
+++ tests/java/TestSample.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestSample {
+    @Test
+    public void testExample() {
+        assertEquals(1, 1);  // Simple test to check if 1 equals 1
+    }
+}
